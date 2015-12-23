26d564d6c36d2ca42e0662adfe738044a4a14df2
Shivam Saxena <shivamfrnd.28@gmail.com>
2015-12-23 23:44:02 +0530

    Removing 'new' action from UserController as new.ejs has been deleted and modals are used instead

--- app/api/controllers/UserController.js
+++ app/api/controllers/UserController.js
@@ -6,9 +6,6 @@
  */
 
 module.exports = {
-	'new':function(req, res){
-		res.view();
-	},
 	create:function(req, res, next){
 		User.create( req.params.all(), function userCreated (err,user){
 			if(err) return next(err);
